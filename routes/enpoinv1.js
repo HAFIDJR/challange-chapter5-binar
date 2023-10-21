01b03e4279ee19427a4e8f778cf42ed7bc3da0f7
Hafid <hafid@gmail.com>
2023-10-21 23:58:16 +0700

    t

--- routes/enpoinv1.js
+++ routes/enpoinv1.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { restrict } = require('../middleware/auth.middlewares');
 const {
+  // 
   createUser,
   getAllUsers,
   getUserDetail,
